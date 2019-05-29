0f08a82e1be6c82a0f22d8099c232186ba8fc874
Musa Abusufyan Yusuf <48918053+sufyan1111@users.noreply.github.com>
2019-05-29 16:39:03 +0100

    index page
    U15FNSCSC003

---
+++ index.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('includes/head.php');?>
+</head>
+
+<body>
+    <!-- ##### Preloader ##### -->
+    <div id="preloader">
+        <i class="circle-preloader"></i>
+    </div>
+
+    <?php include ('includes/header.php');?>
+
+    <!-- ##### Hero Area Start ##### -->
+    <section class="hero-area">
+        <div class="hero-slides owl-carousel">
+
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide bg-img" style="background-image: url(img/bg-img/bg_1.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-center">
+                        <div class="col-12">
+                            <div class="hero-slides-content">
+                                <h4 data-animation="fadeInUp" data-delay="100ms">Old Students</h4>
+                                <h2 data-animation="fadeInUp" data-delay="400ms">Welcome to <br>Federal Government College Minna</h2>
+                                <a href="#about" class="btn academy-btn" data-animation="fadeInUp" data-delay="700ms">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide bg-img" style="background-image: url(img/bg-img/bg_2.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-center">
+                        <div class="col-12">
+                            <div class="hero-slides-content">
+                                <h4 data-animation="fadeInUp" data-delay="100ms">Old Students</h4>
+                                <h2 data-animation="fadeInUp" data-delay="400ms">Welcome to <br>Federal Government College Minna</h2>
+                                <a href="#about" class="btn academy-btn" data-animation="fadeInUp" data-delay="700ms">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Single Hero Slide -->
+            <div class="single-hero-slide bg-img" style="background-image: url(img/bg-img/bg_3.jpg);">
+                <div class="container h-100">
+                    <div class="row h-100 align-items-center">
+                        <div class="col-12">
+                            <div class="hero-slides-content">
+                                <h4 data-animation="fadeInUp" data-delay="100ms">Formally Opened</h4>
+                                <h2 data-animation="fadeInUp" data-delay="400ms">on Monday<br>1<sup>st</sup> January 1979</h2>
+                                <a href="#about" class="btn academy-btn" data-animation="fadeInUp" data-delay="700ms">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ##### Hero Area End ##### -->
+    <!-- ##### Top Feature Area Start ##### -->
+    <div class="top-features-area wow fadeInUp" data-wow-delay="300ms">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="features-content">
+                        <div class="row no-gutters">
+                            <!-- Single Top Features -->
+                            <div class="col-12 col-md-4">
+                                <div class="single-top-features d-flex align-items-center justify-content-center">
+                                    <i class="icon-assistance"></i>
+                                    <h5>Principal</h5>
+                                </div>
+                            </div>
+                            <!-- Single Top Features -->
+                            <div class="col-12 col-md-4">
+                                <div class="single-top-features d-flex align-items-center justify-content-center">
+                                    <i class="icon-assistance"></i>
+                                    <h5>Commttees</h5>
+                                </div>
+                            </div>
+                            <!-- Single Top Features -->
+                            <div class="col-12 col-md-4">
+                                <div class="single-top-features d-flex align-items-center justify-content-center">
+                                    <i class="icon-assistance"></i>
+                                    <h5>Sponsors</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ##### Top Feature Area End ##### -->
+
+       <!-- ##### Staffs ##### -->
+       <div class="academy-courses-area section-padding-100-0">
+        <div class="container">
+            <div class="row">
+                <!-- Staff Details -->
+                <div class="col-12 col-sm-6 col-lg-4">
+                   <div class="single-testimonial-area mb-100 d-flex wow fadeInUp" data-wow-delay="400ms">
+                        <div class="testimonial-thumb">
+                            <img src="img/bg-img/r4.jpg" alt="">
+                        </div>
+                        <div class="course-content">
+                            <h5>Dr. Memunat Manzuma</h5>
+                            <p>2000 - 2014<br><em>Principal F.G.C Minna.</em></p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Staff Details -->
+                <div class="col-12 col-sm-6 col-lg-4">
+                   <div class="single-testimonial-area mb-100 d-flex wow fadeInUp" data-wow-delay="600ms">
+                        <div class="testimonial-thumb">
+                            <img src="img/bg-img/r2.jpg" alt="">
+                        </div>
+                        <div class="course-content">
+                            <h4>Dr. Sulaiman Ladan</h4>
+                            <p>Director of ICT Unit<br><em>Project Manager.</em></p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Staff Details -->
+                <div class="col-12 col-sm-6 col-lg-4">
+                    <div class="single-testimonial-area mb-100 d-flex wow fadeInUp" data-wow-delay="700ms">
+                        <div class="testimonial-thumb">
+                            <img src="img/bg-img/r5.jpeg" alt="">
+                        </div>
+                        <div class="course-content">
+                            <h5>Tech Known Kids</h5>
+                            <p>Training Kids to become the future IT Personals.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Staff Details -->
+                <div class="col-12 col-sm-6 col-lg-4">
+                    <div class="single-testimonial-area mb-100 d-flex wow fadeInUp" data-wow-delay="400ms">
+                        <div class="testimonial-thumb">
+                            <img src="img/bg-img/r3.jpg" alt="">
+                        </div>
+                        <div class="course-content">
+                            <h5>Mr. Ruth Jaja</h5>
+                            <p>2014 - 2018<br><em>Principal F.G.C Minna.</em></p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Staff Details -->
+                <div class="col-12 col-sm-6 col-lg-4">
+                    <div class="single-testimonial-area mb-100 d-flex wow fadeInUp" data-wow-delay="700ms">
+                        <div class="testimonial-thumb">
+                            <img src="img/bg-img/r1.jpg" alt="">
+                        </div>
+                        <div class="course-content">
+                            <h5>Mr. Saliu Mainasara</h5>
+                            <p>Software Engineer<br><em>Web Developer.</em></p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Staff Details -->
+                <div class="col-12 col-sm-6 col-lg-4">
+                   <div class="single-testimonial-area mb-100 d-flex wow fadeInUp" data-wow-delay="700ms">
+                        <div class="testimonial-thumb">
+                            <img src="img/bg-img/r6.png" alt="">
+                        </div>
+                        <div class="course-content">
+                            <h5>Minna Tech Meetup</h5>
+                            <p>Organizing meetups for IT personals<br><em>Minna Branch.</em></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ##### Staffs End ##### -->
+     <!-- ##### About Us Area Start ##### -->
+     <section class="about-us-area mt-50 section-padding-100" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-heading text-center mx-auto wow fadeInUp" data-wow-delay="300ms">
+                        <span>About Us</span>
+                        <h3>Federal Government College Minna Old School</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-6 wow fadeInUp" data-wow-delay="400ms">
+                    <p>Unity schools are a great tool for Nations building. This fact should be obvious because the key objective for setting up the Unity Schools by the Gowon administration was to ensure national integration of the post war generation of Nigerians. Every Unity School was well funded, equipped and had students from all parts of the country. In addition, there were no tuition fees and travel to & fro school was paid for by the Federal Government. From sixties to seventies session, classes were held in temporary classroom blocks built on the college sites.<br/> Our History is therefore incomplete without our past Principals and vice principals of the school up to date. Making our graduates continually being distinguished through the benchmark they make in different sectors and strata of the economy within and outside the country; including careers in business, engineering, law, medicine, arts, and the media.</p>
+                </div>
+                <div class="col-12 col-md-6 wow fadeInUp" data-wow-delay="500ms">
+                    <p>Federal Government College Minna is a co-educational day and boarding, basic and post basic institution owned by the Federal Ministry of Education. The College over the years has been in the forefront of academic excellence within the geographical region and across Nigeria as a whole. An outstanding performance in various Educational, Social and Sporting activities cannot be farfetched. This is as a result of an enabling learning and sporting environment existing in the school, with the existence of modern features such as; a Standard library, School media/ICT laboratory, Multipurpose hall, Tuck shops, Standard Football pitch, Badminton, Volley ball court, and Lawn tennis facility. A good and secured housing plan in form of dormitories/hostels is in place to accommodate the boarding students, under the supervision of caring and experienced house masters and mistresses.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ##### About Us Area End ##### -->
+
+    <!-- ##### Top Popular Courses Area Start ##### -->
+    <div class="top-popular-courses-area section-padding-100-70">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-heading text-center mx-auto wow fadeInUp" data-wow-delay="300ms">
+                        <span>The Best</span>
+                        <h3>Old Head Boys and Girls</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+               
+                <!-- Single Top Popular Course -->
+                <div class="col-12 col-lg-6">
+                    <div class="single-top-popular-course d-flex align-items-center flex-wrap mb-30 wow fadeInUp" data-wow-delay="400ms">
+                        <div class="popular-course-content">
+                            <h5>Aliyu Mohammed</h5>
+                            <span>Head Boy | 1990</span>
+                            <p>Ministry of Land nd Housing.</p>
+                           </div>
+                        <div class="popular-course-thumb bg-img" style="background-image: url(img/bg-img/hb1.jpg);"></div>
+                    </div>
+                </div>
+                
+                <!-- Single Top Popular Course -->
+                <div class="col-12 col-lg-6">
+                    <div class="single-top-popular-course d-flex align-items-center flex-wrap mb-30 wow fadeInUp" data-wow-delay="500ms">
+                        <div class="popular-course-content">
+                            <h5>Memunat Manzuma</h5>
+                            <span>Head Girl | 1990</span>
+                            <p>Software Developer, JDLab Nig LTD Minna.</p>
+                        </div>
+                        <div class="popular-course-thumb bg-img" style="background-image: url(img/bg-img/hg1.jpg);"></div>
+                    </div>
+                </div>
+                  <!-- Single Top Popular Course -->
+                  <div class="col-12 col-lg-6">
+                    <div class="single-top-popular-course d-flex align-items-center flex-wrap mb-30 wow fadeInUp" data-wow-delay="400ms">
+                        <div class="popular-course-content">
+                            <h5>Aliyu Mohammed</h5>
+                            <span>Head Boy | 1990</span>
+                            <p>Ministry of Land nd Housing.</p>
+                           </div>
+                        <div class="popular-course-thumb bg-img" style="background-image: url(img/bg-img/hb1.jpg);"></div>
+                    </div>
+                </div>
+                
+                <!-- Single Top Popular Course -->
+                <div class="col-12 col-lg-6">
+                    <div class="single-top-popular-course d-flex align-items-center flex-wrap mb-30 wow fadeInUp" data-wow-delay="500ms">
+                        <div class="popular-course-content">
+                            <h5>Memunat Manzuma</h5>
+                            <span>Head Girl | 1990</span>
+                            <p>Software Developer, JDLab Nig LTD Minna.</p>
+                        </div>
+                        <div class="popular-course-thumb bg-img" style="background-image: url(img/bg-img/hg1.jpg);"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ##### Top Popular Courses Area End ##### -->
+   
+    <!-- ##### Footer Area Start ##### -->
+        <?php include('includes/footer.php');?>
+    <!-- ##### Footer Area Start ##### -->
+
+    <!-- ##### All Javascript Script ##### -->
+    <!-- jQuery-2.2.4 js -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/bootstrap/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <!-- All Plugins js -->
+    <script src="js/plugins/plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+</body>
+
+</html>
